63f38fbbac2263a44a5dc8200eba84d0cebf329c
Ederene20 <atinhoruben@gmail.com>
2021-08-02 00:53:59 +0100

    fix typo

--- connexion.php
+++ connexion.php
@@ -2,7 +2,7 @@
 
         // Make the connection with the database `recrutementbd`
         try {
-              $bdd = new PDO('mysql:host=localhost;dbname=recrutementbD;charset=utf8', 'root', '');
+              $bdd = new PDO('mysql:host=localhost;dbname=recrutementbd;charset=utf8', 'root', '');
         }
 
         catch (Exception $e) {
